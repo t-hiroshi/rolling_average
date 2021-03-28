6c534e611937731d9e77a8af3bf7d0ee43a8264b
Terauchi Hiroshi <terauchi.hiroshi@cloud-ace.jp>
2021-03-28 17:23:16 +0900

    rolling average の計算を追加

--- parse.py
+++ parse.py
@@ -4,6 +4,12 @@ import seaborn
 
 seaborn.set_style("darkgrid")
 
+
+def rolling_average(row_time: int, num_rolling:int):
+    rolling_time = np.convolve(row_time, np.ones(num_rolling)/num_rolling, mode="same")
+    return rolling_time
+
+
 file_name = "only_time.csv"
 with open(file_name, "r") as f:
     times = f.readlines()
@@ -13,11 +19,16 @@ times.pop(0)
 
 # DNF を削除
 times = [float(s.rstrip("\n")) for s in times if not s.startswith("DNF")]
-
 times = np.array(times)
 
+times_rolling = rolling_average(times, 5)
+
 print(f"mean: {np.mean(times)}")
+
 plt.plot(times)
+plt.plot(times_rolling)
 plt.show()
-plt.hist(times)
+
+""" plt.hist(times, bins=100, density=True)
 plt.show()
+"""
